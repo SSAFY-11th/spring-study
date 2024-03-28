e23285b9f3f0ac06c6db79eaf1fbc17e2e0e3f0f
yub <ajsjdlwj0123@gmail.com>
2024-03-28 21:06:37 +0900

    init security

---
+++ src/main/java/com/ssafy/springstudy/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.ssafy.springstudy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return new WebSecurityCustomizer() {
+            @Override
+            public void customize(WebSecurity web) {
+                web.ignoring()
+                        .requestMatchers("/favicon.ico")
+                        .requestMatchers("/error")
+                        // Ant 스타일의 패턴 예를 들어, "/login", "/logout", "/user/**" 등의 URL 패턴
+                        .requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+            }
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                // authorize객체를 변수로 받아서 url에 따른 인가를 설정해주고 반환한다.
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        // 나머지 url은 인증된 사용자에게만 허용
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/doLogin")
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                        // 로그인 성공시 리다이렉트 url
+                        .defaultSuccessUrl("/")
+                )
+
+
+
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+}
